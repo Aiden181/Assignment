b6d2b7f8c9ca86f17ac5b0a764cfeca6f4fd4825
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-08-25 19:06:36 +0700

    Update Console

--- src/Console.java
+++ src/Console.java
@@ -1,5 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Console {
     public static void main(String[] args) {
+        String input = null;
+        Scanner sc = new Scanner(System.in);
+        List<Lead> leads = new ArrayList<Lead>();
+        List<Interaction> interactions = new ArrayList<Interaction>();
+        do {
+                System.out.println("Managing Leads");
+                System.out.println("1. View all leads");
+                System.out.println("2. Create new lead");
+                System.out.println("3. Delete a lead");
+                System.out.println("4. Update a lead");
+                System.out.println();
+                System.out.println();
+                System.out.println("Managing Interactions");
+                System.out.println("5. View all interactions");
+                System.out.println("6. Create new interaction");
+                System.out.println("7. Delete a interaction");
+                System.out.println("8. Update an interaction");
 
+
+                input = (sc.nextLine());
+                if (input.equals("1")){
+                    System.out.println(leads);
+                }else if (input.equals("2")){
+                    Lead newLead = new Lead();
+                    leads.add(newLead);
+                    System.out.println("Please enter lead's name: ");
+                    newLead.setName(sc.nextLine());
+                    System.out.println("Please enter lead's birthday: ");
+                    // Blank
+                    System.out.println("Please enter lead's gender (M/F): ");
+                    if (sc.nextLine().toLowerCase().equals("m") || sc.nextLine().toLowerCase().equals("f")){
+                        newLead.setGender(true);
+                    }else {
+                        newLead.setGender(false);
+                    }
+                    System.out.println("Please enter lead's phone number: ");
+                    newLead.setPhone(sc.nextLine());
+                    System.out.println("Please enter lead's address: ");
+                    newLead.setAddress(sc.nextLine());
+                }else if (input.equals("3")){
+
+                }else if (input.equals("4")){
+
+                }else if (input.equals("5")){
+                    System.out.println(interactions);
+                }else if (input.equals("6")){
+                    Interaction newInteraction = new Interaction();
+                    interactions.add(newInteraction);
+                    System.out.println("Enter date of interaction: ");
+                    // Blank
+                }else if (input.equals("7")){
+
+                }
+        }while (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5")
+        || input.equals("6") || input.equals("7") || input.equals("8"));
     }
 }
--- src/Lead.java
+++ src/Lead.java
@@ -10,18 +10,6 @@ public class Lead {
     private String email;
     private String address;
 
-    public Lead(String id, String name, Date dob, boolean gender, String phone, String email, String address) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-        this.gender = gender;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-    }
-
-    public Lead() {}
-
     public String getId() {
         return id;
     }
@@ -78,4 +66,16 @@ public class Lead {
         this.address = address;
     }
 
+    @Override
+    public String toString() {
+        return "Lead{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", gender=" + gender +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
