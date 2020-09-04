b6a77cfc23eb77dcc8382460469182bf5716ad5e
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-09-04 13:38:05 +0700

    Update Report and Stats Function - Add Validation for User input in the main menu

--- src/Console.java
+++ src/Console.java
@@ -28,7 +28,16 @@ public class Console {
             System.out.println("8. Delete a interaction");
             System.out.println("9. Report and Statistic");
 
+
+
             input = (sc.nextLine());
+            // User Input Validation
+            while (!(input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5")
+                    || input.equals("6") || input.equals("7") || input.equals("8") || input.equals("9"))){
+                System.out.println("Please enter a valid option: ");
+                input = sc.nextLine();
+            }
+
             switch (input) {
                 case "1": // Print out all created leads
 //                    System.out.println(leads);
@@ -403,108 +412,117 @@ public class Console {
                     }
                     break;
                 case "9":
-                    // Leads Age Report
-                    int countZeroToTen = 0;
-                    int countTenToTwenty = 0;
-                    int countTwentyToSixty = 0;
-                    int countGreaterThanSixty = 0;
+                    System.out.println("1. Leads Age Report");
+                    System.out.println("2. Interaction Potential Report");
+                    System.out.println("3. Interactions by Month Report");
+                    int reportOption = sc.nextInt();
+                    switch (reportOption){
+                        case 1:
+                            // Leads Age Report
+                            int countZeroToTen = 0;
+                            int countTenToTwenty = 0;
+                            int countTwentyToSixty = 0;
+                            int countGreaterThanSixty = 0;
 
-                    for (int i = 0; i < leads.size(); i++) {
-                        if (leads.get(i).getAge() <= 10) {
-                            countZeroToTen++;
-                        } else if (leads.get(i).getAge() > 10 && leads.get(i).getAge() <= 10) {
-                            countTenToTwenty++;
-                        } else if (leads.get(i).getAge() > 20 && leads.get(i).getAge() <= 60) {
-                            countTwentyToSixty++;
-                        } else if (leads.get(i).getAge() > 60) {
-                            countGreaterThanSixty++;
-                        }
+                            for (int i = 0; i < leads.size(); i++) {
+                                if (leads.get(i).getAge() <= 10) {
+                                    countZeroToTen++;
+                                } else if (leads.get(i).getAge() > 10 && leads.get(i).getAge() <= 10) {
+                                    countTenToTwenty++;
+                                } else if (leads.get(i).getAge() > 20 && leads.get(i).getAge() <= 60) {
+                                    countTwentyToSixty++;
+                                } else if (leads.get(i).getAge() > 60) {
+                                    countGreaterThanSixty++;
+                                }
+                            }
+                            System.out.println("Input: No input required");
+                            System.out.println("0 - 10 (years old): " + countZeroToTen);
+                            System.out.println("10 - 20 (years old): " + countTenToTwenty);
+                            System.out.println("20 - 60 (years old): " + countTwentyToSixty);
+                            System.out.println("> 60 (years old): " + countGreaterThanSixty + "\n");
+                            break;
+                        case 2:
+                            // Number of Interactions by potential Report
+                            int countPositive = 0;
+                            int countNegative = 0;
+                            int countNeutral = 0;
+                            for (int i = 0; i < leads.size(); i++){
+                                if (interactions.get(i).getInteractionPot().toLowerCase().contains("positive")){
+                                    countPositive++;
+                                }else if (interactions.get(i).getInteractionPot().toLowerCase().contains("neutral")){
+                                    countNeutral++;
+                                }else if (interactions.get(i).getInteractionPot().toLowerCase().contains("negative")){
+                                    countNegative++;
+                                }
+                            }
+                            System.out.println("Input: Jan 01 2020 - December 31 2020");
+                            System.out.println("Positive: " + countPositive);
+                            System.out.println("Negative: " + countNegative);
+                            System.out.println("Neutral: " + countNeutral + "\n");
+                            break;
+                        case 3:
+                            // Number of Interactions by month report
+                            Calendar cal = Calendar.getInstance();
+                            int countJan = 0;
+                            int countFeb = 0;
+                            int countMar = 0;
+                            int countApr = 0;
+                            int countMay = 0;
+                            int countJun = 0;
+                            int countJul = 0;
+                            int countAug = 0;
+                            int countSep = 0;
+                            int countOct = 0;
+                            int countNov = 0;
+                            int countDec = 0;
+                            for (int i = 0; i < interactions.size(); i++){
+                                cal.setTime(interactions.get(i).getDateOfInteraction());
+                                int month = cal.get(Calendar.MONTH);
+                                if (month == 0){
+                                    countJan++;
+                                }else if (month == 1){
+                                    countFeb++;
+                                }else if (month == 2){
+                                    countMar++;
+                                }else if (month == 3){
+                                    countApr++;
+                                }else if (month == 4){
+                                    countMay++;
+                                }else if (month == 5){
+                                    countJun++;
+                                }else if (month == 6){
+                                    countJul++;
+                                }else if (month == 7){
+                                    countAug++;
+                                }else if (month == 8){
+                                    countSep++;
+                                }else if (month == 9){
+                                    countOct++;
+                                }else if (month == 10){
+                                    countNov++;
+                                }else if (month == 1){
+                                    countDec++;
+                                }
+                            }
+                            System.out.println("Input: Jan 01 2020 - December 31 2020");
+                            System.out.println("Jan 2020: " + countJan);
+                            System.out.println("Feb 2020: " + countFeb);
+                            System.out.println("Mar 2020: " + countMar);
+                            System.out.println("Apr 2020: " + countApr);
+                            System.out.println("May 2020: " + countMay);
+                            System.out.println("Jun 2020: " + countJun);
+                            System.out.println("Jul 2020: " + countJul);
+                            System.out.println("Aug 2020: " + countAug);
+                            System.out.println("Sep 2020: " + countSep);
+                            System.out.println("Oct 2020: " + countOct);
+                            System.out.println("Nov 2020: " + countNov);
+                            System.out.println("Dec 2020: " + countDec);
+                            break;
                     }
-                    System.out.println("Input: No input required");
-                    System.out.println("0 - 10 (years old): " + countZeroToTen);
-                    System.out.println("10 - 20 (years old): " + countTenToTwenty);
-                    System.out.println("20 - 60 (years old): " + countTwentyToSixty);
-                    System.out.println("> 60 (years old): " + countGreaterThanSixty + "\n");
-
-                    // Number of Interactions by potential Report
-                    int countPositive = 0;
-                    int countNegative = 0;
-                    int countNeutral = 0;
-                    for (int i = 0; i < leads.size(); i++){
-                        if (interactions.get(i).getInteractionPot().toLowerCase().contains("positive")){
-                            countPositive++;
-                        }else if (interactions.get(i).getInteractionPot().toLowerCase().contains("neutral")){
-                            countNeutral++;
-                        }else if (interactions.get(i).getInteractionPot().toLowerCase().contains("negative")){
-                            countNegative++;
-                        }
-                    }
-                    System.out.println("Input: Jan 01 2020 - December 31 2020");
-                    System.out.println("Positive: " + countPositive);
-                    System.out.println("Negative: " + countNegative);
-                    System.out.println("Neutral: " + countNeutral + "\n");
-
-                    // Number of Interactions by month report
-                    Calendar cal = Calendar.getInstance();
-                    int countJan = 0;
-                    int countFeb = 0;
-                    int countMar = 0;
-                    int countApr = 0;
-                    int countMay = 0;
-                    int countJun = 0;
-                    int countJul = 0;
-                    int countAug = 0;
-                    int countSep = 0;
-                    int countOct = 0;
-                    int countNov = 0;
-                    int countDec = 0;
-                    for (int i = 0; i < interactions.size(); i++){
-                        cal.setTime(interactions.get(i).getDateOfInteraction());
-                        int month = cal.get(Calendar.MONTH);
-                        if (month == 0){
-                            countJan++;
-                        }else if (month == 1){
-                            countFeb++;
-                        }else if (month == 2){
-                            countMar++;
-                        }else if (month == 3){
-                            countApr++;
-                        }else if (month == 4){
-                            countMay++;
-                        }else if (month == 5){
-                            countJun++;
-                        }else if (month == 6){
-                            countJul++;
-                        }else if (month == 7){
-                            countAug++;
-                        }else if (month == 8){
-                            countSep++;
-                        }else if (month == 9){
-                            countOct++;
-                        }else if (month == 10){
-                            countNov++;
-                        }else if (month == 1){
-                            countDec++;
-                        }
-                    }
-                    System.out.println("Input: Jan 01 2020 - December 31 2020");
-                    System.out.println("Jan 2020: " + countJan);
-                    System.out.println("Feb 2020: " + countFeb);
-                    System.out.println("Mar 2020: " + countMar);
-                    System.out.println("Apr 2020: " + countApr);
-                    System.out.println("May 2020: " + countMay);
-                    System.out.println("Jun 2020: " + countJun);
-                    System.out.println("Jul 2020: " + countJul);
-                    System.out.println("Aug 2020: " + countAug);
-                    System.out.println("Sep 2020: " + countSep);
-                    System.out.println("Oct 2020: " + countOct);
-                    System.out.println("Nov 2020: " + countNov);
-                    System.out.println("Dec 2020: " + countDec);
-
                     break;
             }
         } while (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5")
-                || input.equals("6") || input.equals("7") || input.equals("8"));
+                || input.equals("6") || input.equals("7") || input.equals("8") || input.equals("9"));
     }
 
 }
